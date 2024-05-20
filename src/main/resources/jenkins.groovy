50ae1c526b9dfccded4dd8ff5c2e022360d9374a
Lavendra Kumar Rajput <60038605+lkumarra@users.noreply.github.com>
2024-05-20 19:54:00 +0530

    Updates Jenkins File

--- src/main/resources/jenkins.groovy
+++ src/main/resources/jenkins.groovy
@@ -4,17 +4,12 @@ pipeline {
         string(name: 'browser', defaultValue: 'chrome', description: 'Choose Browser')
         string(name: 'groups', defaultValue: 'all', description: 'Choose Test Group')
         string(name: 'environment', defaultValue: 'jenkins', description: 'Choose Environment')
-        gitParameter(name: 'Branch', type: 'PT_BRANCH', defaultValue: 'main', description: 'Git branch to build from', branch: '', useRepository: 'https://github.com/lkumarra/Selenium_Framework.git', sortMode: 'ASCENDING', selectedValue: 'DEFAULT', quickFilterEnabled: true)
+        gitParameter(name: 'Branch', type: 'PT_BRANCH', defaultValue: 'master', description: 'Git branch to build from', branch: '', useRepository: 'https://github.com/lkumarra/Selenium_Framework.git', sortMode: 'ASCENDING', selectedValue: 'DEFAULT', quickFilterEnabled: true)
     }
     stages {
         stage("Clean Workspace") {
             steps {
                 deleteDir()
-            }
-        }
-        stage('List Branches') {
-            steps {
-                bat 'git ls-remote --heads https://github.com/lkumarra/Selenium_Framework.git'
             }
         }
         stage('Checkout') {
@@ -23,11 +18,9 @@ pipeline {
                     echo "Checking out branch: ${params.Branch}"
                 }
                 checkout([$class: 'GitSCM',
-                          branches: [
-                                  [name: "*/${params.Branch}"]
-                          ],
-                          gitTool: 'Default',
-                          userRemoteConfigs: [[url: 'https://github.com/lkumarra/Selenium_Framework.git']]
+                          branches: [[name: "${params.Branch}"]],
+                          userRemoteConfigs: [[url: 'https://github.com/lkumarra/Selenium_Framework.git']],
+                          extensions: [[$class: 'LocalBranch', localBranch: "${params.Branch}"]]
                 ])
             }
         }

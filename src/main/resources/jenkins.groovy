a825722ec08807f49f2ab1a53f34b1d51e91b346
Lavendra Kumar Rajput <60038605+lkumarra@users.noreply.github.com>
2024-05-20 19:44:05 +0530

    Updated jenkins file

--- src/main/resources/jenkins.groovy
+++ src/main/resources/jenkins.groovy
@@ -1,16 +1,14 @@
-import org.dom4j.Branch
-
 pipeline {
     agent any
     parameters {
         string(name: 'browser', defaultValue: 'chrome', description: 'Choose Browser')
         string(name: 'groups', defaultValue: 'all', description: 'Choose Test Group')
-        string(name: 'environment', defaultValue: 'jenkins', description: 'Choose Test Group')
+        string(name: 'environment', defaultValue: 'jenkins', description: 'Choose Environment')
         gitParameter(name: 'Branch', type: 'PT_BRANCH', defaultValue: 'main', description: 'Git branch to build from', branch: '', useRepository: 'https://github.com/lkumarra/Selenium_Framework.git', sortMode: 'ASCENDING', selectedValue: 'DEFAULT', quickFilterEnabled: true)
     }
     stages {
-        stage("Clean Workspace"){
-            steps{
+        stage("Clean Workspace") {
+            steps {
                 deleteDir()
             }
         }
@@ -18,15 +16,16 @@ pipeline {
             steps {
                 checkout([$class: 'GitSCM',
                           branches: [
-                                  [name: '*/$Branch']
+                                  [name: "*/${params.Branch}"]
                           ],
-                          gitTool:'Default',
-                          userRemoteConfigs: [[url: 'https://github.com/lkumarra/Selenium_Framework.git']]])
+                          gitTool: 'Default',
+                          userRemoteConfigs: [[url: 'https://github.com/lkumarra/Selenium_Framework.git']]
+                ])
             }
         }
         stage('Build') {
             steps {
-                sh "mvn clean test -Denv=%environment% -Dbrowser=%browser% -Dgroups=%groups%"
+                bat "mvn clean test -Denv=${params.environment} -Dbrowser=${params.browser} -Dgroups=${params.groups}"
             }
         }
     }
